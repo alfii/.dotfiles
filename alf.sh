6b1cedd06b05e459901541669b78a7d98e702e5a
Alf-Håkon Pedersen <alf.hakon.pedersen@gmail.com>
2019-05-19 14:02:43 +0200

    alf remove wal from alf.sh

--- alf.sh
+++ alf.sh
@@ -4,15 +4,6 @@ plugins=(
   kubectl completion git composer vi-mode
 )
 
-
-# Import colorscheme from 'wal' asynchronously
-# &   # Run the process in the background.
-# ( ) # Hide shell job control messages.
-(cat ~/.cache/wal/sequences &)
-
-
-
-
 # MISC FUNCTIONS
 function updateDocker(){
     if [ -f "docker/Dockerfile" ]
